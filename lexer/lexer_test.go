8ee8282124eaee059796bfd1b54f9315d20b54c3
Axel Vergult <axel@vergult.io>
2018-08-21 09:39:19 +0200

    Add support for lexing == and != comparison tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -13,14 +13,17 @@ let add = fn(x, y) {
 	x + y;
 };
 let result = add(five, ten);
- !-/*5;
- 5 < 10 > 5;
- 
- if (5 < 10) {
- 	return true;
- } else {
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+	return true;
+} else {
  	return false;
- }
+}
+
+10 == 10;
+10 != 9; 
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -91,6 +94,14 @@ let result = add(five, ten);
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
 	l := New(input)
--- token/token.go
+++ token/token.go
@@ -46,6 +46,9 @@ const (
 	IF       = "IF"
 	ELSE     = "ELSE"
 	RETURN   = "RETURN"
+
+	EQ     = "=="
+	NOT_EQ = "!="
 )
 
 var keywords = map[string]TokenType{
