c3577f46885eb7dc1d49c420a0294d7f8ff775a2
Axel Vergult <axel@vergult.io>
2018-10-11 09:08:40 +0200

    Update REPL to use parser

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"gitlab.com/vergult/monkey/lexer"
-	"gitlab.com/vergult/monkey/token"
+	"gitlab.com/vergult/monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -16,14 +16,28 @@ func Start(in io.Reader, out io.Writer) {
 
 	for {
 		fmt.Printf(PROMPT)
-		if !scanner.Scan() {
+		scanned := scanner.Scan()
+		if !scanned {
 			return
 		}
 
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) > 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
