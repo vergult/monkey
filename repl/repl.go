0c117cc14e59904aca8df2d259ffe1cdb952ff1f
Axel Vergult <axel@vergult.io>
2018-10-15 08:37:17 +0200

    Improve repl error message

--- repl/repl.go
+++ repl/repl.go
@@ -37,6 +37,7 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
