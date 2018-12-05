9b7398a90bd40b145f7a04b856a069f9c1b77896
Axel Vergult <axel@vergult.io>
2018-12-05 09:30:15 +0100

    Implementing function call evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -66,6 +66,11 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		params := node.Parameters
 		body := node.Body
 		return &object.Function{Parameters: params, Body: body, Env: env}
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
 	}
 	return nil
 }
