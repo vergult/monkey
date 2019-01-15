c6a28691c15bec2c8a569805bff832aefd616f92
Axel Vergult <axel@vergult.io>
2019-01-15 09:01:37 +0100

    Add string evaluation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -316,3 +316,15 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 	}
 	return true
 }
+
+func TestStringLiteral(t *testing.T) {
+	input := `"Hello World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not a String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
