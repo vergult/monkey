17e95a56ae0b34eccde92721d022fe2549584327
Axel Vergult <axel@vergult.io>
2019-02-05 08:57:20 +0100

    Add support to lex colon token

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -27,6 +27,7 @@ if (5 < 10) {
 "foobar"
 "foo bar"
 [1, 2];
+{"foo": "bar"}
 `
 	tests := []struct {
 		expectedType    token.TokenType
@@ -113,6 +114,12 @@ if (5 < 10) {
 		{token.INT, "2"},
 		{token.RBRACKET, "]"},
 		{token.SEMICOLON, ";"},
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
+		{token.EOF, ""},
 		{token.EOF, ""},
 	}
 	l := New(input)
