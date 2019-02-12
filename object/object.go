fcbb3c9cca2e596797baf83ef8a4e61d5a26f930
Axel Vergult <axel@vergult.io>
2019-02-12 09:21:18 +0100

    Add object.Hash Inspect method and add evaluation test

--- object/object.go
+++ object/object.go
@@ -174,6 +174,22 @@ func (h *Hash) Type() ObjectType {
 	return HASH_OBJ
 }
 
+func (h *Hash) Inspect() string {
+	var out bytes.Buffer
+	pairs := []string{}
+	for _, pair := range h.Pairs {
+		pairs = append(pairs, fmt.Sprintf("%s: %s", pair.Key.Inspect(), pair.Value.Inspect()))
+	}
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+	return out.String()
+}
+
+type Hashable interface {
+	HashKey() HashKey
+}
+
 type HashKey struct {
 	Type  ObjectType
 	Value uint64
